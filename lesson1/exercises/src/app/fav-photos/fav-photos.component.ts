223a1c1d539ba4910c8c06644e24bf3a39ff81ec
Laura Hamm <theladyofshallot@msn.com>
2021-08-15 18:24:55 -0500

    Fun Times

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Random Images';
+  image1 = 'https://http.cat/502'
+  image2 = 'https://http.cat/405';
+  image3 = 'https://http.cat/403'
 
   constructor() { }
 
